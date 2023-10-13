5a4b6319923d0d388ea860794f13ef89c720569f
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2023-10-13 18:11:15 +0200

    Initialize java spring boot

---
+++ src/main/java/id/itsmeyaw/spotifyoverlay/SpotifyOverlayApplication.java
@@ -0,0 +1,13 @@
+package id.itsmeyaw.spotifyoverlay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyOverlayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyOverlayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/itsmeyaw/spotifyoverlay/SpotifyOverlayApplicationTests.java
@@ -0,0 +1,13 @@
+package id.itsmeyaw.spotifyoverlay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyOverlayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
